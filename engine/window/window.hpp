a9ff96a3aa5de511f7fa3a9315fe7fe6379aa42b
David Kviloria <dkviloria@gmail.com>
2021-06-08 15:34:51 +0400

    added window resize api

--- engine/window/window.hpp
+++ engine/window/window.hpp
@@ -11,99 +11,131 @@
 
 struct Andromeda::Types::WindowConfog
 {
-  char *title;
-  int width, height;
+	char *title;
+	uint32_t width, height;
 };
 
 class Andromeda::Window
 {
 
 private:
-  GLFWwindow *m_WindowID;
-  GLFWmonitor *m_MonitorID;
-  const GLFWvidmode *m_VideoMode;
+	GLFWwindow *m_WindowID;
+	GLFWmonitor *m_MonitorID;
+	const GLFWvidmode *m_VideoMode;
+	Andromeda::Types::WindowConfog m_WindowInfo;
 
 public:
-  Window(Andromeda::Types::WindowConfog *conf)
-  {
+	Window(const Andromeda::Types::WindowConfog &conf)
+	{
 
-    Andromeda::Log::SetPrefix("Window");
+		Andromeda::Log::SetPrefix("Window");
 
-    {
-      int status = (int)glfwInit() == GLFW_TRUE;
-      ASSERT(status, "Error while initializing GLFW", NULL);
-    }
+		{
+			int status = (int)glfwInit() == GLFW_TRUE;
+			ASSERT(status, "Error while initializing GLFW", NULL);
+		}
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_FALSE);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_FALSE);
 
-    m_MonitorID = glfwGetPrimaryMonitor();
-    m_VideoMode = glfwGetVideoMode(m_MonitorID);
-    m_WindowID = glfwCreateWindow(conf->width, conf->height, static_cast<const char *>(conf->title), nullptr, nullptr);
+		m_MonitorID = glfwGetPrimaryMonitor();
+		m_VideoMode = glfwGetVideoMode(m_MonitorID);
+		m_WindowID = glfwCreateWindow(conf.width, conf.height, static_cast<const char *>(conf.title), nullptr, nullptr);
 
-    glfwMakeContextCurrent(m_WindowID);
-    glfwSwapInterval(1);
+		glfwMakeContextCurrent(m_WindowID);
+		glfwSwapInterval(1);
 
-    {
-      int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-      ASSERT(status, "Failed to initialize OpenGL loader", NULL);
-    }
+		{
+			int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+			ASSERT(status, "Failed to initialize OpenGL loader", NULL);
+		}
 
-    Andromeda::Log::Print("glfw window has been initialzed");
-  }
+		m_WindowInfo = conf;
+		Andromeda::Log::Print("glfw window has been initialzed");
+	}
 
-  static GLFWkeyfun KeyHandler(GLFWwindow *window, GLFWkeyfun callback)
-  {
-    return glfwSetKeyCallback(window, callback);
-  }
+	static Andromeda::Types::WindowConfog GetWindowInfo()
+	{
+		return get_window_info_impl();
+	}
 
-  static int ShouldClose(GLFWwindow *handle)
-  {
-    return glfwWindowShouldClose(handle);
-  }
+	static void SetWindowInfo(uint32_t width, uint32_t height)
+	{
+		Get().set_window_info_impl(width, height);
+	}
 
-  static double TimeNow(void)
-  {
-    return glfwGetTime();
-  }
+	static GLFWkeyfun KeyHandler(GLFWwindow *window, GLFWkeyfun callback)
+	{
+		return glfwSetKeyCallback(window, callback);
+	}
 
-  static void PullEvents()
-  {
-    glfwPollEvents();
-  }
+	static int ShouldClose(GLFWwindow *handle)
+	{
+		return glfwWindowShouldClose(handle);
+	}
 
-  static void SwapBuffers(GLFWwindow *window)
-  {
-    glfwSwapBuffers(window);
-  }
+	static double TimeNow(void)
+	{
+		return glfwGetTime();
+	}
 
-  static void Destory(GLFWwindow *window)
-  {
-    glfwDestroyWindow(window);
-    glfwTerminate();
-  }
+	static void PullEvents()
+	{
+		glfwPollEvents();
+	}
 
-  void HideCursor() const
-  {
-    glfwSetInputMode(m_WindowID, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-  }
+	static void SwapBuffers(GLFWwindow *window)
+	{
+		glfwSwapBuffers(window);
+	}
 
-  inline GLFWwindow *const GetId() const
-  {
-    return m_WindowID;
-  }
+	static void Destory(GLFWwindow *window)
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
 
-  inline GLFWmonitor *const GetMonitorId() const
-  {
-    return m_MonitorID;
-  }
+	void HideCursor() const
+	{
+		glfwSetInputMode(m_WindowID, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+	}
 
-  inline const GLFWvidmode *const GetVideoMode() const
-  {
-    return m_VideoMode;
-  }
+	inline GLFWwindow *const GetId() const
+	{
+		return m_WindowID;
+	}
+
+	inline GLFWmonitor *const GetMonitorId() const
+	{
+		return m_MonitorID;
+	}
+
+	inline const GLFWvidmode *const GetVideoMode() const
+	{
+		return m_VideoMode;
+	}
+
+	Window() = default;
+
+private:
+	static Andromeda::Types::WindowConfog get_window_info_impl()
+	{
+		return Get().m_WindowInfo;
+	}
+
+	void set_window_info_impl(uint32_t width, uint32_t height)
+	{
+		m_WindowInfo.width = width;
+		m_WindowInfo.height = height;
+	}
+
+	static Andromeda::Window &Get()
+	{
+		static Andromeda::Window s_Instance;
+		return s_Instance;
+	}
 };
 
 #endif // __ENGINE_WINDOW__
--- sandbox/include/app.hpp
+++ sandbox/include/app.hpp
@@ -11,71 +11,72 @@
 struct Sandbox
 {
 public:
-  int m_Width, m_Height;
-  double m_DeltaTime;
-  double m_ElapsedTime = Andromeda::Window::TimeNow();
+	int m_Width, m_Height;
+	double m_DeltaTime;
+	double m_ElapsedTime = Andromeda::Window::TimeNow();
 
-  Sandbox()
-  {
-    // Configure Window
-    Andromeda::Types::WindowConfog conf{"Andromeda", 1080, 720};
+	Sandbox()
+	{
+		// Configure Window
+		Andromeda::Types::WindowConfog conf{"Andromeda", 1080, 720};
 
-    // Create new Window
-    m_Window = new Andromeda::Window(&conf);
+		// Create new Window
+		m_Window = new Andromeda::Window(conf);
 
-    // Attach Window Key handler
-    Andromeda::Window::KeyHandler(GetWidnowId(), Sandbox::KeyHandler);
-  }
+		// Attach Window Key handler
+		Andromeda::Window::KeyHandler(GetWidnowId(), Sandbox::KeyHandler);
+	}
 
-  inline GLFWwindow *const GetWidnowId() const
-  {
-    return m_Window->GetId();
-  }
+	inline GLFWwindow *const GetWidnowId() const
+	{
+		return m_Window->GetId();
+	}
 
-  inline Andromeda::Window *Window() const
-  {
-    return m_Window;
-  }
+	inline Andromeda::Window *Window() const
+	{
+		return m_Window;
+	}
 
-  void UpdateTime(double ct)
-  {
-    m_DeltaTime = ct - m_ElapsedTime;
-    m_ElapsedTime = ct;
-  }
+	void UpdateTime(double ct)
+	{
+		m_DeltaTime = ct - m_ElapsedTime;
+		m_ElapsedTime = ct;
+	}
 
-  void Resize()
-  {
-    glfwGetWindowSize(GetWidnowId(), &m_Width, &m_Height);
-  }
+	void Resize()
+	{
+		glfwGetWindowSize(GetWidnowId(), &m_Width, &m_Height);
+		m_Window->SetWindowInfo(m_Width, m_Height);
+	}
 
-  static void Draw(int width, int height);
+	static void Draw(int width, int height);
 
-  static void BaseRenderer(int width, int height)
-  {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	static void BaseRenderer(int width, int height)
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+		glViewport(0, 0, width, height);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
 
-    float aspect = (float)width / (float)height;
-    glOrtho(-aspect, aspect, -1, 1, 0.00001, -100000);
-  }
-  /*
+		float aspect = (float)width / (float)height;
+		glOrtho(-aspect, aspect, -1, 1, 0.00001, -100000.f);
+	}
+	/*
 	 *
 	 * WINDOW Callbacks
 	 *
 	 */
-  static void KeyHandler(GLFWwindow *window, int key, int scancode, int action, int mods)
-  {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    {
-      glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-  }
+	static void KeyHandler(GLFWwindow *window, int key, int scancode, int action, int mods)
+	{
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
+		}
+	}
 
 private:
-  Andromeda::Window *m_Window;
+	Andromeda::Window *m_Window;
 };
 
 #endif // __SANDBOX_APP__
